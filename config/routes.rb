d2f92a7a24d3400446e53693ea299a7d6e7a71a3
Rob Ogus <rob.ogus@gmail.com>
2015-11-10 13:30:55 -0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing', to: 'static_pages#landing_page' 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
